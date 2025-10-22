64b095d789bb1aeef54925aa75c19405fbeb79ed
Juh Cipriani <julia_cipriani150@estudante.sesisenai.org.br>
2025-10-22 13:54:32 -0300

    Criando a base do projeto.

---
+++ src/test/java/com/exame/projeto_exame/ProjetoExameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exame.projeto_exame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoExameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
