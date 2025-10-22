300f438f2c372077f5b64b714ee4c3b304989751
Juh Cipriani <julia_cipriani150@estudante.sesisenai.org.br>
2025-10-22 16:19:58 -0300

    Controller
    Terminando de fazer os controllers

--- src/main/java/com/exame/projeto_exame/controller/ExameController.java
+++ src/main/java/com/exame/projeto_exame/controller/ExameController.java
@@ -1,5 +1,68 @@
 package com.exame.projeto_exame.controller;
 
-public class ExameController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.exame.projeto_exame.model.Exame;
+import com.exame.projeto_exame.service.ExameService;
+import com.exame.projeto_exame.service.LaboratorioService;
+import com.exame.projeto_exame.service.PacienteService;
+
+@Controller
+@RequestMapping("/exames")
+public class ExameController {
+    @Autowired
+    private ExameService exameService;
+
+    @Autowired
+    private PacienteService pacienteService;
+
+    @Autowired
+    private LaboratorioService laboratorioService;
+
+    @GetMapping
+    public String listarExame(Model model) {
+        List<Exame> exame = exameService.listarExame();
+        model.addAttribute("exame", exame);
+        return "lista_exame";
+    }
+
+    @GetMapping("/novo")
+    public String NovaExame(Model model) {
+
+        model.addAttribute("exame", new Exame());
+        model.addAttribute("paciente", pacienteService.listarPaciente());
+        model.addAttribute("laboratorio", laboratorioService.listarLaboratorio());
+        return "form_exame";
+    }
+
+    @PostMapping
+    public String salvarExame(@ModelAttribute Exame exame) {
+        exameService.salvarExame(exame);
+        return "lista_exame";
+    }
+
+    @GetMapping("/{id}/editar")
+    public String editarExame(@PathVariable Long id, Model model) {
+        Exame exame = exameService.buscarExame(id);
+        model.addAttribute("exame", exame);
+        model.addAttribute("paciente", pacienteService.listarPaciente());
+        model.addAttribute("laboratorio", laboratorioService.listarLaboratorio());
+        return "form_exame";
+
+    }
+
+    @GetMapping("/{id}/deletar")
+    public String deletarExame(@PathVariable Long id) {
+        exameService.deletarExame(id);
+        return "lista_exame";
+    }
 }
--- src/main/java/com/exame/projeto_exame/controller/LaboratorioController.java
+++ src/main/java/com/exame/projeto_exame/controller/LaboratorioController.java
@@ -1,5 +1,51 @@
 package com.exame.projeto_exame.controller;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import com.exame.projeto_exame.model.Laboratorio;
+import com.exame.projeto_exame.service.LaboratorioService;
+
+@Controller
+@RequestMapping("/laboratorios")
 public class LaboratorioController {
 
+    @Autowired
+    private LaboratorioService laboratorioService;
+
+    @GetMapping
+    public String listarLaboratorio(Model model) {
+        List<Laboratorio> laboratorio = laboratorioService.listarLaboratorio();
+        model.addAttribute("laboratorio", laboratorio);
+        return "lista_laboratorio";
+    }
+
+    @GetMapping("/novo")
+    public String novoLaboratorio(Model model) {
+        model.addAttribute("laboratorio", new Laboratorio());
+        return "form_laboratorio";
+    }
+
+    @PostMapping 
+    public String salvarLaboratorio(@ModelAttribute Laboratorio laboratorio) {
+        laboratorioService.salvarLaboratorio(laboratorio);
+        return "redirect:/laboratorios";
+    }
+
+    @GetMapping("/{id}/editar")
+    public String editarLaboratorio(@PathVariable Long id, Model model) {
+        Laboratorio laboratorio = laboratorioService.buscarLaboratorio(id);
+        model.addAttribute("laboratorio", laboratorio);
+        return "form_laboratorio";
+    }
+
+    @GetMapping("/{id}/deletar")
+    public String deletarLaboratorio(@PathVariable Long id) {
+        laboratorioService.deletarLaboratorio(id);
+        return "redirect:/laboratorios";
+    }
 }
+
